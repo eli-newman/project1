15be5b1d04fb43c05a1f7a8e903fc6dbabcf50c7
elijn04 <elijn04@gmail.com>
2025-11-10 11:36:36 -0700

    add main so program can run

---
+++ src/dataviewer2/Main.java
@@ -0,0 +1,14 @@
+package dataviewer2;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Main entry point for the DataViewer application.
+ */
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        String dataFile = "Data/GlobalLandTemperaturesByState.csv";
+        // String dataFile = "Data/sample.csv";  // Use for testing
+        new ApplicationController(dataFile);
+    }
+}
