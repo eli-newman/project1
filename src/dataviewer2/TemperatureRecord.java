96607246aea1d6f3eaa542bf46742a4aa2ffbc17
Bread <97198020+Breadiguess@users.noreply.github.com>
2025-11-10 22:56:29 -0700

    Add country field to TemperatureRecord class

--- src/dataviewer2/TemperatureRecord.java
+++ src/dataviewer2/TemperatureRecord.java
@@ -9,7 +9,7 @@ public class TemperatureRecord {
     private final Integer month;
     private final Double temperature;
     private final String state;
-    
+    private final String country;
     /**
      * Create a new temperature record.
      * 
@@ -18,11 +18,13 @@ public class TemperatureRecord {
      * @param temperature Temperature in Celsius
      * @param state State name
      */
-    public TemperatureRecord(Integer year, Integer month, Double temperature, String state) {
+    public TemperatureRecord(Integer year, Integer month, Double temperature, String state, String country) {
         this.year = year;
         this.month = month;
         this.temperature = temperature;
         this.state = state;
+
+        this.country = country;
     }
     
     public Integer getYear() {
@@ -40,11 +42,15 @@ public class TemperatureRecord {
     public String getState() {
         return state;
     }
-    
+    public String getCountry(){
+        return country;
+    }
     @Override
     public String toString() {
         return String.format("TemperatureRecord[%d-%02d, %s, %.2f°C]", 
             year, month, state, temperature);
     }
+
+	
 }
 
