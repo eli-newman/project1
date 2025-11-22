8bad0aa06caa40a250b01369b3f8401cb2302c1c
corbinlew <corbinlew@gmail.com>
2025-11-21 21:37:46 -0800

    Update MenuModeState.java to use observer

--- src/dataviewer2/MenuModeState.java
+++ src/dataviewer2/MenuModeState.java
@@ -2,9 +2,16 @@ package dataviewer2;
 
 import edu.du.dudraw.Draw;
 
-public class MenuModeState implements AppModeState {
+public class MenuModeState implements AppModeState, Observer {
+	private boolean updated = true;
+	
 	public void update(ApplicationState appState, PlotRenderer plotRenderer, Draw window, DataViewerData plotData)
 	{
+		if (!updated)
+		{
+			return;
+		}
+		updated = false;
 		plotRenderer.drawMainMenu(window, 
                 appState.getSelectedCountry(),
                 appState.getSelectedState(),
@@ -16,5 +23,33 @@ public class MenuModeState implements AppModeState {
 	public void handleInput(int key, DataViewerApp dataViewerApp, ApplicationState appState)
 	{
 		dataViewerApp.handleMenuInput(key);
+	}
+
+	//having 5 methods to recieve data is a lot more flexible than having one, because it means we can have different updates if specific values have changed.
+	
+	@Override
+	public void onCountryChanged(String newValue) {
+		updated = true;
+		
+	}
+
+	@Override
+	public void onStateChanged(String newValue) {
+		updated = true;
+	}
+
+	@Override
+	public void onStartYearChanged(int newValue) {
+		updated = true;
+	}
+
+	@Override
+	public void onEndYearChanged(int newValue) {
+		updated = true;
+	}
+
+	@Override
+	public void onVisualizationChanged(String newValue) {
+		updated = true;
 	}
 }
